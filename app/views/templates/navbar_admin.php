7c6ee95e8f9796681c3905af340c3153ab4eaab3
KIM <m.absharhakim@gmail.com>
2025-05-03 17:15:36 +0700

    Add contact form and styling for improved user interaction

---
+++ app/views/templates/navbar_admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
+
+</head>
+
+<header>
+    <div class="logo">
+        <img src="<?= BASEURL; ?> /img/logo.svg" alt="Logo Posyandu" ;>
+    </div>
+    <nav>
+        <ul class="nav-list">
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#about">Input</a></li>
+            <li><a href="#blog">Delete</a></li>
+            <li><a href="#">List</a></li>
+        </ul>
+    </nav>
+</header>
+<body>

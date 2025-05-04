1eb54bda7a60cc68c63dddd83d8a659a405f1a10
dika ramadani <dikabangkok21@gmail.com>
2025-05-04 21:05:56 +0700

    Add detail method and data retrieval by ID in Data_model

--- app/models/Data_model.php
+++ app/models/Data_model.php
@@ -20,4 +20,10 @@ class Data_model
        $this ->db->query('SELECT * FROM ' . $this->table);
        return $this->db->resultSet();
     }
+    public function getDataById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id = :id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }

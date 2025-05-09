b3ab68e19a843f618d1617d086d4bbbf8ebc9411
KIM <m.absharhakim@gmail.com>
2025-05-09 15:48:39 +0700

    Refactor Admin controller methods: streamline view calls and remove unnecessary data handling.

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -3,14 +3,38 @@
 class Admin extends Controller {
     public function index()
     {
-        $data['judul'] = 'admin';
-        $data['nama'] = $this->model('User_model')->getUser();
-        $this->view('templates/navbar_admin', $data);
-        $this->view('hero_admin/index', $data);
-        $this->view('hero_admin/delete', $data);
-        $this->view('hero_admin/input', $data);
-        $this->view('hero_admin/list', $data);
-        $this->view('hero_admin/search', $data);
+
+        $this->view('templates/navbar_admin');
+        $this->view('hero_admin/index');
         $this->view('templates/footer');
     }
+        public function delete()
+    {
+
+        $this->view('templates/navbar_admin');
+        $this->view('hero_admin/delete');
+        $this->view('templates/footer');
+    }
+        public function input()
+    {
+
+        $this->view('templates/navbar_admin');
+        $this->view('hero_admin/input');
+        $this->view('templates/footer');
+    }
+        public function list()
+    {
+
+        $this->view('templates/navbar_admin');
+        $this->view('hero_admin/list');
+        $this->view('templates/footer');
+    }
+        public function search()
+    {
+
+        $this->view('templates/navbar_admin');
+        $this->view('hero_admin/search');
+        $this->view('templates/footer');
+    }
+
 }

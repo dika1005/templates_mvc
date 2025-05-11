8fc4148f2d68ff05eb272537064cd961d35f60e4
dika ramadani <dikabangkok21@gmail.com>
2025-05-12 01:15:56 +0700

    Refactor admin controller and update navbar links: add missing methods and ensure proper routing for admin actions

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -8,4 +8,34 @@ class admin extends Controller {
         $this->view('admin/index');
         $this->view('templates/footer');
     }
+    public function delete()
+    {
+        $data['judul'] = 'Delete admin';
+        $this->view('templates/navbar', $data);
+        $this->view('admin/delete');
+        $this->view('templates/footer');
+    }
+    public function list()
+    {
+        $data['judul'] = 'List admin';
+        $this->view('templates/navbar', $data);
+        $this->view('admin/list');
+        $this->view('templates/footer');
+    }
+
+    public function search()
+    {
+        $data['judul'] = 'Search admin';
+        $this->view('templates/navbar', $data);
+        $this->view('admin/search');
+        $this->view('templates/footer');
+    }
+
+    public function input()
+    {
+        $data['judul'] = 'Input admin';
+        $this->view('templates/navbar', $data);
+        $this->view('admin/input');
+        $this->view('templates/footer');
+    }
 }
--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -19,8 +19,8 @@
       </div>
       <div class="nav-toggle" onclick="toggleMenu()">☰</div>
       <ul class="nav-links">
-        <li><a href="search.php"><i class="fas fa-search"></i> Search Data</a></li>
-        <li><a href="delete.php"><i class="fas fa-trash"></i> Delete Data</a></li>
+        <li><a href="<?= BASEURL; ?>/admin/search"><i class="fas fa-search"></i> Search Data</a></li>
+        <li><a href="<?= BASEURL; ?>/admin/delete"><i class="fas fa-trash"></i> Delete Data</a></li>
         <li><a href="admin.php"><i class="fas fa-pen"></i> Input Data</a></li>
         <li><a href="list.php"><i class="fas fa-list"></i> List Data</a></li>
       </ul>

cf1468428acd19d22b6e38bff7fea32daa8e38dc
mdprstyo <prasetyodeko240@gmail.com>
2025-05-12 14:27:28 +0700

    Add user index view and corresponding styles: create user index page with a data table layout and enhance styling for better presentation

---
+++ app/views/user/index.php
@@ -0,0 +1,16 @@
+<main>
+    <h1>Selamat Datang, Nama Pengguna!</h1>
+    <p>Silakan pilih menu di navbar untuk mengakses fitur yang tersedia.</p>
+
+    <div class="card">
+      <table class="data-table">
+        <tr><th>NIK</th><td></td></tr>
+        <tr><th>Nama</th><td></td></tr>
+        <tr><th>Alamat</th><td></td></tr>
+        <tr><th>Jenis Kelamin</th><td></td></tr>
+        <tr><th>Umur</th><td></td></tr>
+        <tr><th>Tinggi Badan</th><td></td></tr>
+        <tr><th>Berat Badan</th><td></td></tr>
+      </table>
+    </div>
+  </main>

c9d38ac3614ca475dbe9deb90047823194404b31
dika ramadani <dikabangkok21@gmail.com>
2025-05-18 02:36:45 +0700

    Update navbar titles for consistency: change "Admin Panel" to "Halakan User" and vice versa

--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Halakan User</title>
+  <title>Admin Panel</title>
 <link rel="stylesheet" href="<?= BASEURL; ?>/css/navbar1.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
